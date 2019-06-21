830881726b732e900b6611b1f22bf86b9e1e33cb
Daniel Barde <mrbarde2k@gmail.com>
2019-06-21 01:08:00 +0100

    Authenticated endpoints with passport

--- configs/passport.js
+++ configs/passport.js
@@ -1,33 +1,29 @@
 const passport = require('passport');
 const JWTStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const User = require('../models/User');
+const db = require('../models');
 const jwtConfig = require('../configs/jwt');
+const { User } = db;
 
 /**
  * Here we use passports' jwt strategy to
  * get the user from the token passed in
  * the authorizations header.
  */
-passport.use( new JWTStrategy(
+passport.use(new JWTStrategy(
   {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: jwtConfig.secret
   },
-  (payload, done) => {
+  async (payload, done) => {
     // fund user bearing the user id on the payload
-    User.findOne({ where: { id: payload.user_id }})
-        .then((user) => {
-          // check if the user does not exist
-          if(!user){
-          // return an error response
-          done(new Error('invalid token'), false)
-          // stop executing function
-          return;
-        }
-        // continue request
-        done(null, user);
-      });
+    const user = await User.findOne({ where: { id: payload.user_id } });
+
+    // check if the user does not exist
+    if (!user) return done(new Error('invalid token'), false);
+
+    // continue request
+    done(null, user);
   }
 ));
 
